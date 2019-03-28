935a8c042b30dd3660aa932b3db94534e5057c98
unknown <1053685823@qq.com>
2019-03-28 18:40:29 +0800

    some change

--- baoge/src/page/home/home.vue
+++ baoge/src/page/home/home.vue
@@ -2,9 +2,9 @@
   <div class="content">
     <head-top signin-up='home'></head-top>
     <div class='total'>
-      <div class='carousel'>
+      <div class='carousel-message'>
         <ul>
-          <li></li>
+          <li v-for="carouselData in carouselList" :key="carouselData.id" ><img :src="carouselData.imgLinks"></li>
         </ul>
       </div>
     </div>
@@ -13,41 +13,67 @@
 
 <script>
 import headTop from '../../common/header/head'
+import { getCarouselList } from '../../http/carousel'
 export default {
   name: 'home',
   data () {
     return {
+      carouselList: []
     }
   },
   components: {
     headTop
+  },
+  mounted () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData () {
+      getCarouselList()
+        .then(res => {
+          console.log(res)
+          this.carouselList = res.data
+        })
+        .catch(err => {
+          this.$message.error(err.message)
+        })
+    }
   }
 }
 </script>
 
 <style>
+  *{
+    margin: 0;
+    padding: 0;
+  }
   .content{
     width: 100%;
     height: 100%
   }
   .total{
-    width: 100%;
+    height: 100%;
     margin: 0 auto;
   }
-  .carousel{
-    width: 100%;
-    height:450px;
+  .carousel-message{
+    height: 445px;
     background: yellow;
   }
-  .carousel ul{
+  .carousel-message ul{
     width: 100%;
-    height: 450px;
+    height: 445px;
     margin: 0;
     padding: 0;
+    overflow: hidden;
   }
-  .carousel li{
+  .carousel-message ul li{
     width: 100%;
     height: 100%;
     list-style: none;
+    float: right;
+  }
+  .carousel-message ul li img{
+    width: 100%;
+    height: 100%;
   }
 </style>
